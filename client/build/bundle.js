87f8a62c34b3117f0b35d6acaecda4741fb95992
Jay Schwane <jrschwane@uwalumni.com>
2017-06-29 23:17:04 -0500

    Adds client build files

---
+++ client/build/bundle.js
@@ -0,0 +1,287 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+/* controller.js -- connects our model and view */
+/* jshint esversion:6, devel:true */
+
+var Controller = function () {
+    function Controller(model, view) {
+        _classCallCheck(this, Controller);
+
+        this.model = model;
+        this.view = view;
+    }
+
+    _createClass(Controller, [{
+        key: 'render',
+        value: function render(data) {
+            this.view.render(data);
+        }
+    }, {
+        key: 'setView',
+        value: function setView() {
+            var _this = this;
+
+            console.log('controller setView fired');
+            this.model.getProfiles().then(function (data) {
+                return _this.render(data);
+            });
+        }
+    }]);
+
+    return Controller;
+}();
+
+exports.default = Controller;
+
+},{}],2:[function(require,module,exports){
+'use strict';
+
+var _model = require('./model');
+
+var _model2 = _interopRequireDefault(_model);
+
+var _util = require('./util');
+
+var _view = require('./view');
+
+var _view2 = _interopRequireDefault(_view);
+
+var _controller = require('./controller');
+
+var _controller2 = _interopRequireDefault(_controller);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } } /* jshint esversion:6, browser:true */
+
+var App = function App() {
+    _classCallCheck(this, App);
+
+    var model = new _model2.default();
+    var view = new _view2.default();
+
+    this.controller = new _controller2.default(model, view);
+};
+
+var app = new App();
+
+var setView = function setView() {
+    app.controller.setView();
+};
+
+(0, _util.$on)(window, 'load', setView);
+
+},{"./controller":1,"./model":3,"./util":6,"./view":7}],3:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }(); /* jshint esversion:6, devel:true */
+/*
+   T3BC1RPPH = chingu central
+   T4ZS2DFAA = chingu rhinos
+   T5518TKBR = extemporaneous
+*/
+
+var _service = require('./service');
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Model = function () {
+    function Model() {
+        _classCallCheck(this, Model);
+    }
+
+    /* fetch user profiles for a given team ID
+    */
+
+
+    _createClass(Model, [{
+        key: 'getProfiles',
+        value: function getProfiles() {
+            return (0, _service.getJSON)('/api/team/T3BC1RPPH').then(function (profiles) {
+                console.log('profiles from model.js', profiles);
+                return profiles;
+            });
+        }
+    }]);
+
+    return Model;
+}();
+
+exports.default = Model;
+
+},{"./service":4}],4:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+/* jshint esversion:6, devel:true, browser:true */
+
+///* a get JSON function using callbacks
+//*/
+//const getJSON = (url, success, error) => {
+//    const xhr = new XMLHttpRequest();
+//    xhr.onreadystatechange = () => {
+//        if (xhr.readyState === 4) {
+//            if (xhr.status === 200) {
+//                success(JSON.parse(xhr.responseText));
+//            } else {
+//                error(xhr.responseText);
+//            }
+//        }
+//    };
+//    xhr.open('GET', url);
+//    xhr.send();
+//};
+
+
+/* a get JSON function using promises
+*/
+var getJSON = function getJSON(url) {
+    var xhr = new XMLHttpRequest();
+    return new Promise(function (resolve, reject) {
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    resolve(JSON.parse(xhr.responseText));
+                } else {
+                    reject(xhr.responseText);
+                }
+            }
+        };
+        xhr.open('GET', url);
+        xhr.send();
+    });
+};
+
+exports.getJSON = getJSON;
+
+},{}],5:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _templateObject = _taggedTemplateLiteral(['\n    <tr>\n        <td>', '</td>\n        <td>', '</td>\n        <td>', '</td>\n        <td>', '</td>\n        <td>', '</td>\n    </tr>\n'], ['\n    <tr>\n        <td>', '</td>\n        <td>', '</td>\n        <td>', '</td>\n        <td>', '</td>\n        <td>', '</td>\n    </tr>\n']),
+    _templateObject2 = _taggedTemplateLiteral(['\n    <table class="table">\n        <thead>\n            <th>Team</th>\n            <th>Team Member</th>\n            <th>Last Updated</th>\n            <th>Total Skills</th>\n            <th>Skills</th>\n        </thead>\n        <tbody>\n            ', '\n        </tbody>\n    </table>\n'], ['\n    <table class="table">\n        <thead>\n            <th>Team</th>\n            <th>Team Member</th>\n            <th>Last Updated</th>\n            <th>Total Skills</th>\n            <th>Skills</th>\n        </thead>\n        <tbody>\n            ', '\n        </tbody>\n    </table>\n']);
+
+function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+/* jshint esversion:6, devel:true */
+
+/* utility method for easy templating of repeating html elements
+ * such as lists.
+ *
+ * 'tagged template literals' are strings that are first passed
+ * through a custom function which returns the processed string.
+ *
+ * @params  [array]  literal     [array of all the literal secti
+ * @params  [array]  ...cooked   [rest param: all the proccessed expressions]
+ * @returns [string]             [the processed string]
+ */
+var html = function html(literalsArr) {
+    for (var _len = arguments.length, cooked = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+        cooked[_key - 1] = arguments[_key];
+    }
+
+    var result = '';
+
+    cooked.forEach(function (cook, i) {
+        var lit = literalsArr[i];
+        if (Array.isArray(cook)) {
+            cook = cook.join('');
+        }
+        result += lit;
+        result += cook;
+    });
+    result += literalsArr[literalsArr.length - 1];
+    return result;
+};
+
+/* template that returns a 'tr'
+*/
+var table_row = function table_row(profile) {
+    return html(_templateObject, profile.team_domain, profile.user_name, profile.timestamp.slice(3, 15), profile.skills.length, profile.skills.join(', '));
+};
+
+/* template that loops over the array of profiles adding a 'tr' for each
+*/
+var table_template = function table_template(data) {
+    return html(_templateObject2, data.map(function (profile) {
+        return table_row(profile);
+    }));
+};
+
+//console.log(table_template(temp_data));
+
+exports.table_template = table_template;
+
+},{}],6:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+/* library of helper functions */
+/* jshint esversion:6 */
+
+/* register handler to an event emitted by a target */
+var $on = function $on(target, event, handler) {
+    return target.addEventListener(event, handler);
+};
+
+exports.$on = $on;
+
+},{}],7:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }(); /* view.js 
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        handles rendering and DOM events.
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        uses the template strings, feeds them data, and adds their output
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        to the right DOM elements.
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     */
+/* jshint esversion:6, browser: true */
+
+var _template = require('./template');
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var View = function () {
+    function View() {
+        _classCallCheck(this, View);
+
+        this.el = document.getElementById('target');
+    }
+
+    _createClass(View, [{
+        key: 'render',
+        value: function render(data) {
+            console.log('data from view', data);
+            this.el.innerHTML = (0, _template.table_template)(data);
+        }
+    }]);
+
+    return View;
+}();
+
+exports.default = View;
+
+},{"./template":5}]},{},[2]);
