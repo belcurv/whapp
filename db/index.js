a725863c247daafc38c3720ff8659bfa46b6e70c
Jay Schwane <jrschwane@uwalumni.com>
2017-06-29 23:24:07 -0500

    is it dot env?

--- db/index.js
+++ db/index.js
@@ -1,7 +1,6 @@
 /* jshint esversion:6, node: true */
 
-const dotenv = require('dotenv').config(),
-      dbuser = process.env.DB_UNAME,
+const dbuser = process.env.DB_UNAME,
       dbpwd  = process.env.DB_PWD;
 
 module.exports = {
