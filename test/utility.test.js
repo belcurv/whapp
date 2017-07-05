b411d49bd3c076b035f939dc885643c8719b123b
peter martinson <peter martinson>
2017-07-04 22:44:04 -0400

    Try to get Sinon to fake a Mongoose call

--- test/utility.test.js
+++ test/utility.test.js
@@ -1,6 +1,8 @@
-var expect = require('chai').expect;
-var util   = require('../utilities/database_utilities');
-var Profile = require('../models/profile_model');
+const mongoose = require('mongoose'),
+      sinon = require('sinon'),
+      expect = require('chai').expect,
+      util   = require('../utilities/database_utilities'),
+      Profile = require('../models/profile_model');
 
 
 describe('database_utilities', function() {
@@ -17,5 +19,27 @@ describe('database_utilities', function() {
   it('should contain fetchAllSkills()', function() {
     expect(util.fetchAllSkills).to.exist;
     expect(util.fetchAllSkills).to.be.a('function');
+  });
+  it('should do something awesome', function() {
+    var stub = sinon.stub(mongoose.Model, 'find');
+    var callback = sinon.spy();
+    var test_profile = {
+            'team_id'     : 'team_id',
+            'team_domain' : 'team_domain',
+            'channel_id'  : 'channel_id',
+            'channel_name': 'channel_name',
+            'user_id'     : 'user_id',
+            'user_name'   : 'user_name',
+            'postText'    : 'postText',
+            'timestamp'   : 'timestamp',
+            'skills'      : ['skill 1', 'skill 2']
+    };
+    stub.returns(test_profile);
+    util.fetchAllSkills(callback);
+
+    expect(callback.called).to.be.true;
+
+
+
   });
 });
--- utilities/database_utilities.js
+++ utilities/database_utilities.js
@@ -1,12 +1,18 @@
 var util = {};
+const Profile = require('../models/profile_model');
 
 util.testFunction = function() {
   return 1;
 }
 
+/**
+ * gets the list of unique skills from the database
+*/
 util.fetchAllSkills = function(callback) {
+  console.log('inside the function');
   Profile.find( (err, profiles) => {
     if (err) throw err;
+    console.log('inside find');
 
     let all_skills = [];
     
