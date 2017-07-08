6f8d9112963350db3eb3d63470979c8b89bc3d1a
Jay Schwane <jrschwane@uwalumni.com>
2017-07-07 22:10:43 -0500

    Add simple D3 SVG demo chart

---
+++ client/src/js/chart_thing.js
@@ -0,0 +1,28 @@
+/* jshint esversion:6 */
+
+/* https://bl.ocks.org/mbostock/3887235 */
+
+import * as d3 from 'd3';
+
+const chart_thing = (data, targetEl, configMap) => {
+    
+    // set defaults in case no config object received
+    const width  = configMap.width  || 800;
+    const height = configMap.height || 400;
+    
+    d3.select(targetEl).append('svg')
+          .attr('width',  width)
+          .attr('height', height)
+        .selectAll('p')
+        .data(data)
+        .enter()
+        .append('rect')
+          .attr('x', (d, i) => width / (data.length - 1) * i)
+          .attr('y', (d) => height - d.count)
+          .attr('width', width / data.length)
+          .attr('height', (d) => d.count)
+          .style('fill', 'steelblue');
+    
+};
+
+export { chart_thing };
--- client/src/js/view.js
+++ client/src/js/view.js
@@ -1,22 +1,30 @@
-/* view.js
-   handles rendering and DOM events.
-   uses the template strings, feeds them data, and adds their output
-   to the right DOM elements.
-*/
 /* jshint esversion:6, browser: true */
 
 import { table_template }  from './template';
 import { header_template } from './template';
-import { piechart } from './piechart';
+import { chart_thing }     from './chart_thing';
 
 export default class View {
     
     constructor() {
-        this.target = document.getElementById('target');
+        this.target    = document.getElementById('target');
+        this.svgTarget = document.getElementById('svg');
     }
     
     render(data) {
-        piechart(data.skills);
+        
+        // empty the chart container first ...
+        this.svgTarget.innerHTML = '';
+        
+        /* chart_thing builds the D3 graphic
+         *
+         * @param   [array]   skills      [the skill:count map array]
+         * @param   [object]  svgTarget   [target DOM element for D3]
+         * @param   [object]  config      [config map]
+        */
+        chart_thing(data.skills, this.svgTarget, {width: 800, height: 100});
+        
+        // empty the table before repopulating...
         this.target.innerHTML = '';
         this.target.innerHTML += header_template(data.team);
         this.target.innerHTML += table_template(data.skills);
