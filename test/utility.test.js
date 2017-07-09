e29fbd68bbbd35bd91dc4e357071902c01608efd
Peter Martinson <peter.j.martinson@gmail.com>
2017-07-08 22:58:27 -0400

    Cover almost completely with tests
    I'm not sure how to write a meaningful test for the test normalizer yet.
    Maybe it needs to be broken up somehow...

--- test/utility.test.js
+++ test/utility.test.js
@@ -51,8 +51,8 @@ describe('Database Functions: ', function() {
 
   it('fetchAllProfiles should fetch all the profiles', function() {
     var expectedModels = sample_profiles;
-    Profile.find.yields(null, expectedModels);
     var callback = sinon.stub();
+    Profile.find.yields(null, expectedModels);
     util.fetchAllProfiles(callback);
     sinon.assert.calledWith(callback, expectedModels);
   });
@@ -66,10 +66,51 @@ describe('Database Functions: ', function() {
     sinon.assert.calledWith(callback, expectedResults);
   });
 
+  it('fetchProfileByUserId should fetch one profile', function() {
+    var expectedModel = sample_profiles[0];
+    var callback = sinon.stub();
+    Profile.findOne.yields(null, expectedModel);
+    util.fetchProfileByUserId(sample_profiles[0].user_id, callback);
+    sinon.assert.calledWith(callback, expectedModel);
+  });
+
+  it('fetchProfileByUserName should fetch one profile', function() {
+    var expectedModel = sample_profiles[0];
+    var callback = sinon.stub();
+    Profile.findOne.yields(null, expectedModel);
+    util.fetchProfileByUserName(sample_profiles[0].user_name, callback);
+    sinon.assert.calledWith(callback, expectedModel);
+  });
+
+  it('fetchProfilesByTeamId should fetch several profiles', function() {
+    var expectedModels = sample_profiles;
+    var team_id = 'T3BC1RPPH';
+    var callback = sinon.stub();
+    Profile.find.yields(null, expectedModels);
+    util.fetchProfilesByTeamId(team_id, callback);
+    sinon.assert.calledWith(callback, expectedModels);
+  });
+
+  it('fetchProfilesByTeamName should fetch several profiles', function() {
+    var expectedModels = sample_profiles;
+    var team_name = 'chingucentral';
+    var callback = sinon.stub();
+    Profile.find.yields(null, expectedModels);
+    util.fetchProfilesByTeamId(team_name, callback);
+    sinon.assert.calledWith(callback, expectedModels);
+  });
+
+  // it('normalizeSkillsAgainstDataDictionary should change a skill', function() {
+  //   var expectedModels = sample_profiles;
+  //   var expectedResults = normalized_profiles;
+  //   sinon.stub(util, 'fetchAllProfiles');
+  //   util.fetchAllProfiles.restore();
+  // });
+
 });
       
 
-/*________________________________ Standards ________________________________*/
+/*__________________________ Standards __________________________*/
 
 var sample_profiles = [ {
 	team_id: 'T3BC1RPPH',
@@ -78,10 +119,10 @@ var sample_profiles = [ {
 	channel_name: 'directmessage',
 	user_id: 'U5YEHNYBS',
 	user_name: 'linus-br',
-	postText: 'Javascript, NodeJS, Java, Fitness, Riding',
+	postText: 'Javascript, NodeJS, Java, Fitness, Riding, backend',
 	timestamp: 'Tue Jun 27 2017 17:58:47 GMT+0000 (UTC)',
 	__v: 0,
-	skills: [ 'JavaScript', 'Node.js', 'Java', 'Fitness', 'Riding' ]
+	skills: [ 'JavaScript', 'Node.js', 'Java', 'Fitness', 'Riding', 'backend']
 },
 {
 	team_id: 'T3BC1RPPH',
@@ -96,7 +137,7 @@ var sample_profiles = [ {
 	skills: [ 'PHP', 'JavaScript', 'ReactJS', 'CSS', 'HTML' ]
 } ];
 
-var all_skills = ["CSS", "Fitness", "HTML", "Java", "JavaScript", "Node.js", "PHP", "ReactJS", "Riding"]
+var all_skills = ["backend", "CSS", "Fitness", "HTML", "Java", "JavaScript", "Node.js", "PHP", "ReactJS", "Riding"]
 
 var getAllSkills = function() {
   let all_skills = [];
--- utilities/database_utilities.js
+++ utilities/database_utilities.js
@@ -25,9 +25,12 @@ util.fetchAllSkills = function(callback) {
   });
 }
 
+/**
+ * Makes a sorted array of unique skills from an array of profiles
+*/
 util.makeSortedArray = function(profiles) {
   let all_skills = [];
-  profiles.forEach((profile) => {
+  profiles.forEach( (profile) => {
     profile["skills"].forEach(function(skill) {
       if ( all_skills.indexOf(skill) < 0 ) {
         all_skills.push(skill);
@@ -37,4 +40,112 @@ util.makeSortedArray = function(profiles) {
   return all_skills.sort();
 }
 
+/**
+ * get a single profile
+ *
+ * @params {string} the profile.user_id
+*/
+util.fetchProfileByUserId = function(user_id, callback) {
+  Profile.findOne({ user_id : user_id }, (err, profile) => {
+    if (err) throw err;
+    callback(profile);
+  });
+}
+
+/**
+ * get a single profile
+ *
+ * @params {string} the profile.user_name
+*/
+util.fetchProfileByUserName = function(user_name, callback) {
+  Profile.findOne({ user_name : user_name }, (err, profile) => {
+    if (err) throw err;
+    callback(profile);
+  });
+}
+
+/**
+ * get all profiles of a given team
+ *
+ * @params {string} the profiles.team_id
+*/
+util.fetchProfilesByTeamId = function(team_id, callback) {
+  Profile.find({ team_id : team_id }, (err, profiles) => {
+    if (err) throw err;
+    callback(profiles);
+  });
+}
+
+/**
+ * get all profiles of a given team
+ *
+ * @params {string} the profiles.team_name
+*/
+util.fetchProfilesByTeamName = function(team_domain, callback) {
+  Profile.find({ team_domain : team_domain }, (err, profiles) => {
+    if (err) throw err;
+    callback(profiles);
+  });
+}
+
+/**
+ * updates all profiles in database with normalized skills
+*/
+util.normalizeSkillsAgainstDataDictionary = function() {
+  fetchAllProfiles( (profiles) => {
+    let count_modified = 0;
+    for (let profile in profiles) {
+      let old_skills = profiles[profile].skills,
+          new_skills = [],
+          _id = profiles[profile]._id,
+          user_name = profiles[profile].user_name,
+          changed = 0;
+
+      // populate new_skills array with normalized skills
+      old_skills.forEach( (skill) => {
+        if ( skill == getNormalizedSkill(skill) ) {
+          new_skills.push(skill);
+        } else {
+          new_skills.push(getNormalizedSkill(skill));
+          changed = 1;
+        }
+      });
+
+      // update the profile
+      replaceUserSkills(_id, new_skills, (err, log) => {
+        if (err) throw err;
+      });
+
+      if ( changed ) {
+        count_modified++;
+      }
+
+    } // for
+
+    if ( count_modified ) {
+      console.log(`${count_modified} profiles have been updated.\n`);
+    } else {
+      console.log('no profiles updated\n');
+    }
+    displayAvailableCommands();
+  });
+}
+
 module.exports = util;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
