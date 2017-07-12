c295d8a9a12398acd21fd7a1d0a319fc431b31c3
Peter Martinson <peter.j.martinson@gmail.com>
2017-07-11 23:29:28 -0400

    Make the object more explicit
    Instead of declaring each function with `util.`, make the functions all
    methods of the object in one declaration.  Application of DRY.

--- utilities/database_utilities.js
+++ utilities/database_utilities.js
@@ -1,118 +1,112 @@
-const Profile = require('../models/profile_model');
-let util = {};
+const Profile         = require('../models/profile_model'),
+      data_dictionary = require('../data_dictionary/dataDictionary');
 
-util.testFunction = function() {
-  return 1;
-}
+module.exports = {
 
-util.fetchAllProfiles = function(callback) {
-  Profile.find( (err, profiles) => {
-    if (err) throw err;
-    callback(profiles);
-  });
-}
+  testFunction : function() {
+    return 1;
+  },
 
-util.fetchAllSkills = function(callback) {
-  Profile.find( (err, profiles) => {
-    if (err) throw err;
-    callback(this.makeSortedArray(profiles));
-  });
-}
-
-util.makeSortedArray = function(profiles) {
-  let all_skills = [];
-  profiles.forEach( (profile) => {
-    profile["skills"].forEach(function(skill) {
-      if ( all_skills.indexOf(skill) < 0 ) {
-        all_skills.push(skill);
-      }
+  fetchAllProfiles : function(callback) {
+    Profile.find( (err, profiles) => {
+      if (err) throw err;
+      callback(profiles);
     });
-  });
-  return all_skills.sort();
-}
+  },
 
-util.fetchProfileByUserId = function(user_id, callback) {
-  Profile.findOne({ user_id : user_id }, (err, profile) => {
-    if (err) throw err;
-    callback(profile);
-  });
-}
+  fetchAllSkills : function(callback) {
+    Profile.find( (err, profiles) => {
+      if (err) throw err;
+      callback(this.makeSortedArray(profiles));
+    });
+  },
 
-util.fetchProfileByUserName = function(user_name, callback) {
-  Profile.findOne({ user_name : user_name }, (err, profile) => {
-    if (err) throw err;
-    callback(profile);
-  });
-}
-
-util.fetchProfilesByTeamId = function(team_id, callback) {
-  Profile.find({ team_id : team_id }, (err, profiles) => {
-    if (err) throw err;
-    callback(profiles);
-  });
-}
-
-util.fetchProfilesByTeamName = function(team_domain, callback) {
-  Profile.find({ team_domain : team_domain }, (err, profiles) => {
-    if (err) throw err;
-    callback(profiles);
-  });
-}
-
-util.normalizeSkillsAgainstDataDictionary = function() {
-
-  fetchAllProfiles( (profiles) => {
-    let count_modified = 0;
-    for (let profile in profiles) {
-      let old_skills = profiles[profile].skills,
-          new_skills = [],
-          _id = profiles[profile]._id,
-          user_name = profiles[profile].user_name,
-          changed = 0;
-
-      // populate new_skills array with normalized skills
-      old_skills.forEach( (skill) => {
-        if ( skill == getNormalizedSkill(skill) ) {
-          new_skills.push(skill);
-        } else {
-          new_skills.push(getNormalizedSkill(skill));
-          changed = 1;
+  makeSortedArray : function(profiles) {
+    let all_skills = [];
+    profiles.forEach( (profile) => {
+      profile["skills"].forEach(function(skill) {
+        if ( all_skills.indexOf(skill) < 0 ) {
+          all_skills.push(skill);
         }
       });
+    });
+    return all_skills.sort();
+  },
 
-      // update the profile
-      replaceUserSkills(_id, new_skills, (err, log) => {
-        if (err) throw err;
-      });
+  fetchProfileByUserId : function(user_id, callback) {
+    Profile.findOne({ user_id : user_id }, (err, profile) => {
+      if (err) throw err;
+      callback(profile);
+    });
+  },
 
-      if ( changed ) {
-        count_modified++;
+  fetchProfileByUserName : function(user_name, callback) {
+    Profile.findOne({ user_name : user_name }, (err, profile) => {
+      if (err) throw err;
+      callback(profile);
+    });
+  },
+
+  fetchProfilesByTeamId : function(team_id, callback) {
+    Profile.find({ team_id : team_id }, (err, profiles) => {
+      if (err) throw err;
+      callback(profiles);
+    });
+  },
+
+  fetchProfilesByTeamName : function(team_domain, callback) {
+    Profile.find({ team_domain : team_domain }, (err, profiles) => {
+      if (err) throw err;
+      callback(profiles);
+    });
+  },
+
+  normalizeSkillsAgainstDataDictionary : function() {
+
+    fetchAllProfiles( (profiles) => {
+      let count_modified = 0;
+      for (let profile in profiles) {
+        let old_skills = profiles[profile].skills,
+            new_skills = [],
+            _id = profiles[profile]._id,
+            user_name = profiles[profile].user_name,
+            changed = 0;
+
+        // populate new_skills array with normalized skills
+        old_skills.forEach( (skill) => {
+          if ( skill == getNormalizedSkill(skill) ) {
+            new_skills.push(skill);
+          } else {
+            new_skills.push(getNormalizedSkill(skill));
+            changed = 1;
+          }
+        });
+
+        // update the profile
+        replaceUserSkills(_id, new_skills, (err, log) => {
+          if (err) throw err;
+        });
+
+        if ( changed ) {
+          count_modified++;
+        }
+
+      } // for
+
+      if ( count_modified ) {
+        console.log(`${count_modified} profiles have been updated.\n`);
+      } else {
+        console.log('no profiles updated\n');
       }
-
-    } // for
-
-    if ( count_modified ) {
-      console.log(`${count_modified} profiles have been updated.\n`);
-    } else {
-      console.log('no profiles updated\n');
-    }
-    displayAvailableCommands();
-  });
+      displayAvailableCommands();
+    });
+  }
 }
 
-/**
- * Updates a single profile's skills property
-*/
 function replaceUserSkills(_id, replacement_skills, callback) {
   Profile.update({ _id : _id }, { $set: {skills : replacement_skills} }, callback);
 }
 
-/**
- * check a skill against the data dictionary
- *
- * @params {string} the skill to check against dictionary
- * @returns {string} either the passed skill, or the data dictionary's skill
-*/
 function getNormalizedSkill(current_skill) {
   var match_skill = current_skill;
   for (let skill in data_dictionary) {
@@ -124,7 +118,6 @@ function getNormalizedSkill(current_skill) {
   }
   return match_skill;
 };
-module.exports = util;
 
 
 
